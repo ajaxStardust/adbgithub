4069e0b3ec1c0ee4da05ed5d1e04cb9b6ec49bf3
ajaxStardust <94220040+ajaxStardust@users.noreply.github.com>
2022-08-09 11:22:02 -0400

    updated footer for adb_small_greyscale.png
    updated small aesthetic things wasting time

--- inc/footer.inc.php
+++ inc/footer.inc.php
@@ -30,8 +30,7 @@ foreach($footParse as $fPkey => $fPval){
 $footDir = __FILE__;
 $footReplace = '';
 $footHost = str_replace($footFile, $footReplace, $footRoot);
-// var_dump(get_defined_vars());
-// print_r(get_defined_vars());
+ 
 ?>
 
 	<div id="footer">	<!-- 	^	#footer 	^	-->
@@ -39,9 +38,12 @@ $footHost = str_replace($footFile, $footReplace, $footRoot);
 				<?php print $backlinksHtml; ?>
 
 	<div class="vertMid">						<!--	^	.vertMid	^	-->
-
-		<p id="toTop" class="floatRt"><a class="intraNav" href="#header"><img src="css/arrow-up.gif" alt="jump to top" />[ Top ]
-	</a></p>
+		<img src="css/adb_small_greyscale.png style="width:100%;height:100%;" class="floatLt" />
+		<p id="toTop" class="floatRt">
+			<a class="intraNav" href="#header">
+			<img src="css/arrow-up.gif" alt="jump to top" />[ Top ]
+			</a>
+		</p>
 		<p id="searchrec">Search privately: <a href="https://duckduckgo.com/goodies" title="Duck Duck Go does not keep tabs on your privacy. This is what you want!"><strong>Duck-Duck-Go-for-it!</strong></a>&trade;</p>
 	</div>										<!--	$	.vertMid	$	-->
 
