4c01577406f36dd31c09d797788e867007b8cf0b
ajaxStardust <94220040+ajaxStardust@users.noreply.github.com>
2023-10-22 10:23:19 -0400

    included fonts thing

--- commonfonts.php
+++ commonfonts.php
@@ -1,29 +1,24 @@
 <?php
-require 'public/inc/sf_dochead_inc.php';
 
+    require 'public/inc/sf_dochead_inc.php';
+    
 ?>
 
 <body>
-	<!-- 	^ 	ID:PANELWIDTH 	^ 	-->
-	<div id="panelwidth">
-	<?php
 
-	// include 'public/inc/sf_prelude_inc.php';
+    <!--    ^   ID:PANELWIDTH   ^   -->
+    <div id="panelwidth">
+        <div id="jstest"></div>
+<?php
+ 
 
- require 'public/inc/sf_primaryfontlist_inc.php';
+    require 'public/inc/sf_primaryfontlist_inc.php'; 
 
-	// include 'public/inc/sf_tipsbetween_inc.php';
+    require 'public/inc/sf_customarea_inc.php'; 
 
-	require 'public/inc/sf_customarea_inc.php';
-
-	// include 'public/inc/sf_footer_inc.php';
-
-	// include 'public/inc/sf_credits_inc.php';
-
-	?>
-
-<!-- div id="jsPreview"></div -->
-	</div>
-	<!-- 	$ 	ID:PANELWIDTH 	$ 	-->
+?>
+    </div>
+    <!--    $   ID:PANELWIDTH   $   -->
+    
 </body>
 </html>
--- public/inc/sf_dochead_inc.php
+++ public/inc/sf_dochead_inc.php
@@ -2,19 +2,19 @@
 <?php
 // MAKE AN ARRAY OF THE MULTIPLE FONT-FAMILIES:
 if(isset($_GET['selected_font_name'])) {
-	$htmlSelect_GETselectedFont=$_GET['selected_font_name'];
-	$htmlDivId_DisplayFontTxt='#newfontfam';
-	// $htmlSelect_GETselectedFont = $htmlSelect_GETselectedFont.$htmlDivId_DisplayFontTxt;
-	$htmlProp_jsOnload = 'onload="location.href='.$_SERVER["PHP_SELF"].'?selected_font_name='.$htmlSelect_GETselectedFont.$htmlDivId_DisplayFontTxt.'"';
-	$htmlInlineStyle_fontText = " style=\"color:#dFF;\" ";
+    $htmlSelect_GETselectedFont=$_GET['selected_font_name'];
+    $htmlDivId_DisplayFontTxt='#newfontfam';
+    // $htmlSelect_GETselectedFont = $htmlSelect_GETselectedFont.$htmlDivId_DisplayFontTxt;
+    $htmlProp_jsOnload = 'onload="location.href='.$_SERVER["PHP_SELF"].'?selected_font_name='.$htmlSelect_GETselectedFont.$htmlDivId_DisplayFontTxt.'"';
+    $htmlInlineStyle_fontText = " style=\"color:#dFF;\" ";
 } else {
-	$htmlProp_jsOnload="";
+    $htmlProp_jsOnload="";
 }
 /**
-*	@var array _customfams
-*		Keys are manually matched with values for purpose of use as
-*		CSS classes such that corresponding Font Family names will use
-*		array Key as CSS class value thereby styling appropriately
+*   @var array _customfams
+*       Keys are manually matched with values for purpose of use as
+*       CSS classes such that corresponding Font Family names will use
+*       array Key as CSS class value thereby styling appropriately
 **/
 $defaultFontsArray = array('liberSans'=>"Liberation Sans",
 'liberSerif'=>"Liberation Serif",
@@ -50,54 +50,47 @@ $defaultFontsArray = array('liberSans'=>"Liberation Sans",
 'exceptMSSans'=>"MS Sans-Serif");
 asort($defaultFontsArray);
 $htmlSelect_FontName = "<select  onchange=\"submit()\" name=\"selected_font_name\"> \n".
-					"<option value=\"\"> -- Select New Font -- </option> \n";
+                    "<option value=\"\"> -- Select New Font -- </option> \n";
 foreach($defaultFontsArray as $fontsArrayKey_asSelectOptionValue => $fontsArrayValue_asSelectOptionDisplay) {
-	$htmlSelect_FontName .= "<option value=\"".$fontsArrayKey_asSelectOptionValue."\">".$fontsArrayValue_asSelectOptionDisplay."</option> \n";
+    $htmlSelect_FontName .= "<option value=\"".$fontsArrayKey_asSelectOptionValue."\">".$fontsArrayValue_asSelectOptionDisplay."</option> \n";
 }
 $htmlSelect_FontName .= "</select> \n";
 if(empty($_GET['selected_font_name'])){
-	if(isset($_POST['customfontentry'])){
-		$httpRequest_fontIndex = $_POST['customfontentry'];
-	}
-	else{
-<<<<<<< HEAD
-		$httpRequest_fontIndex = "fantasy";
-=======
-		$httpRequest_fontIndex = "fantasy";	
->>>>>>> a5636a2cd6d8c43da9e4d060ab7e8791e9c73ddf
-	}
+    if(isset($_POST['customfontentry'])){
+        $httpRequest_fontIndex = $_POST['customfontentry'];
+    }
+    else{
+        $httpRequest_fontIndex = "fantasy";
+    }
 }
 else
-	{
+    {
 $httpRequest_fontIndex = $_GET['selected_font_name'];
 }
 foreach($defaultFontsArray as $fontsArrayKey_asCssClass => $fontsArrayValue_asFontName)
-		{
-	if($fontsArrayKey_asCssClass==$httpRequest_fontIndex) {
-	$httpRequest_ucfirstFontName = ucfirst($fontsArrayValue_asFontName);
-	$htmlAnchor_displayDetails = "<a id=\"declarAnchor\" href=\"#".$fontsArrayKey_asCssClass."\">Details</a>";
-	break;
-	}
+        {
+    if($fontsArrayKey_asCssClass==$httpRequest_fontIndex) {
+    $httpRequest_ucfirstFontName = ucfirst($fontsArrayValue_asFontName);
+    $htmlAnchor_displayDetails = "<a id=\"declarAnchor\" href=\"#".$fontsArrayKey_asCssClass."\">Details</a>";
+    break;
+    }
 }
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en" >
 <head profile="http://WordPressCenter.net/profile.html">
 <title>Windows and Mac Font Equivalents - Browser Panel</title>
-	<meta http-equiv="Content-Style-Type" content="text/css" />
-	<meta http-equiv="content-type" content="application/xhtml+xml;charset=utf-8" />
+    <meta http-equiv="Content-Style-Type" content="text/css" />
+    <meta http-equiv="content-type" content="application/xhtml+xml;charset=utf-8" />
 <meta name="author" content="NoviceNotes&#x2122;" />
 <meta name="description" content="Fonts common to all versions of Windows with Mac and Linux equivalents listed as a tool for creating web pages." />
 <meta name="keywords" content="windows,mac,linux,fonts,common,similar,equivalent,standard,usual,typical,frequent" />
 <!-- link rel="stylesheet" href="assets/css/safefonts.css" type="text/css" media="screen" charset="UTF-8" -->
 <!-- tempcss -->
-<link rel="stylesheet" href="assets/css/safefontstyle_cssToolbox_akelPadEdit.css" type="text/css" media="screen" charset="UTF-8" />
+<link rel="stylesheet" href="assets/css/safefontstyle.css" type="text/css" media="screen" charset="UTF-8" />
 <link rel="shortcut icon" href="http://WordPressCenter.net/favicon.ico" />
-<<<<<<< HEAD
+
 <script type="text/javascript" src="assets/js/thetoggler.js"></script>
-=======
-<script type="text/javascript" src="js/thetoggler.js"></script>
->>>>>>> a5636a2cd6d8c43da9e4d060ab7e8791e9c73ddf
 <style type="text/css">
 /*<![CDATA[*/
 
